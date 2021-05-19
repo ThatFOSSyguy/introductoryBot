2b0ff27f3b42418190b5f070f4e350ba31caf4f9
Mihir Vagal <77693447+ThatFOSSyguy@users.noreply.github.com>
2021-05-19 13:49:20 +0530

    Create main.py

---
+++ main.py
@@ -0,0 +1,22 @@
+# Please read the README.md first
+import discord
+import os
+from pingingTheBot import keeping_alive # Pinging the bot
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+  print('I have logged in as {0.user}'.format(client))
+
+@client.event
+async def on_message(message):
+  if message.author == client.user:
+    return
+
+  intrMsg = """__""" # <--Enter the introduction there
+  if message.content.startswith('!intro'): # You can change the message content to call the bot
+    await message.channel.send(intrMsg)
+
+keeping_alive()
+client.run(os.getenv('Token'))
