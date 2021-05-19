8d0bc1f025dfbed1dd6b871a803add34a040d601
Mihir Vagal <77693447+ThatFOSSyguy@users.noreply.github.com>
2021-05-19 14:00:27 +0530

    Add files via upload

---
+++ pingingTheBot.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from threading import Thread
+
+app = Flask('')
+
+@app.route("/")
+def home():
+  return "Hello there!"
+
+def run():
+  app.run(host='',port='')# Enter the host and port there
+
+def keeping_alive():
+  t = Thread(target=run)
+  t.start()
+  
